a41fea9f81509c47966e3bdea43d7916b2abe013
Egor Tornikov <egor.tornikov@siemens.com>
2021-12-22 17:13:21 +0300

    try fix autotest

--- internal/handlers/handlerChi_test.go
+++ internal/handlers/handlerChi_test.go
@@ -27,9 +27,9 @@ func testRequest(t *testing.T, ts *httptest.Server, method, path string, body []
 	require.NoError(t, err)
 
 	respBody, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-
 	defer resp.Body.Close()
+
+	require.NoError(t, err)
 
 	return resp, string(respBody)
 }
