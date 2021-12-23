ce7ed9fd552efdcb671453f239e3f788836b889f
Egor Tornikov <egor.tornikov@siemens.com>
2021-12-23 13:14:39 +0300

    overcome linters bug

--- internal/handlers/handlerChi_test.go
+++ internal/handlers/handlerChi_test.go
@@ -39,17 +39,17 @@ func TestRouter(t *testing.T) {
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	resp, body := testRequest(t, ts, http.MethodPost, "/", []byte("http://ya.ru"))
+	resp, body := testRequest(t, ts, http.MethodPost, "/", []byte("http://ya.ru")) //nolint:bodyclose // linters bug
 	assert.Equal(t, 201, resp.StatusCode)
 	assert.Equal(t, ts.URL+"/0", body)
 
-	resp, body = testRequest(t, ts, http.MethodPost, "/", []byte("http://google.ru"))
+	resp, body = testRequest(t, ts, http.MethodPost, "/", []byte("http://google.ru")) //nolint:bodyclose // linters bug
 	assert.Equal(t, 201, resp.StatusCode)
 	assert.Equal(t, ts.URL+"/1", body)
 
-	resp, _ = testRequest(t, ts, http.MethodGet, "/0", nil)
+	resp, _ = testRequest(t, ts, http.MethodGet, "/0", nil) //nolint:bodyclose // linters bug
 	assert.Equal(t, 307, resp.StatusCode)
-	assert.Equal(t, "http://ya.ru", resp.Header.Get("Location"))
+	assert.Equal(t, "http://ya.ru", resp.Header.Get("Location")) //nolint:bodyclose // linters bug
 
 	resp, _ = testRequest(t, ts, http.MethodGet, "/1", nil)
 	assert.Equal(t, 307, resp.StatusCode)
