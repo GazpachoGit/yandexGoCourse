f880d6b3ba361bc4ea08c8de315156d775588271
Egor Tornikov <egor.tornikov@siemens.com>
2022-01-25 17:01:04 +0300

    add contentType check

--- internal/middlewares/compress.go
+++ internal/middlewares/compress.go
@@ -18,19 +18,37 @@ func (w gzipWriter) Write(b []byte) (int, error) {
 
 func CompressHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rEncoding := r.Header.Get("Accept-Encoding")
-		if !strings.Contains(rEncoding, "gzip") {
-			next.ServeHTTP(w, r)
-			return
-		}
-		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-		defer gz.Close()
+		rEncoding := strings.Join(r.Header.Values("Accept-Encoding"), ",")
+		rContentType := strings.Join(r.Header.Values("Content-Type"), ",")
 
-		w.Header().Set("Content-Encoding", "gzip")
-		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+		if strings.Contains(rEncoding, "gzip") && checkContentType(rContentType) {
+			gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer gz.Close()
+
+			w.Header().Set("Content-Encoding", "gzip")
+			next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+		}
+		next.ServeHTTP(w, r)
 	})
+}
+
+func checkContentType(rContentType string) bool {
+	contentTypes := []string{
+		"application/javascript",
+		"application/json",
+		"text/css",
+		"text/html",
+		"text/plain",
+		"text/xml",
+	}
+	for _, ct := range contentTypes {
+		if strings.Contains(rContentType, ct) {
+			return true
+		}
+	}
+	return false
 }
